60c7f09ab83e2df44d89e660ba14f2811c8092a5
fourside <fourside@gmail.com>
2024-03-07 23:26:03 +0900

    generation logic / chore

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,55 @@ import { ChangeEvent, useState } from "react";
 import classes from "./App.module.css";
 
 type Mode = "edit" | "progress";
-type Cell = "-" | "x";
+type DeadCell = "x";
+type AliveCell = "-";
+type Cell = DeadCell | AliveCell;
 
-function createCells(width: number, height: number): Cell[][] {
-  return Array.from({ length: height }, () =>
-    Array.from({ length: width }, () => "-"),
+type AroundCell = Cell | undefined;
+type Around8Cells = [
+  AroundCell,
+  AroundCell,
+  AroundCell,
+  AroundCell,
+  AroundCell,
+  AroundCell,
+  AroundCell,
+  AroundCell,
+];
+
+function isAliveCell(cell: Cell | undefined): boolean {
+  return cell === "-";
+}
+
+function deadOrAlive(target: Cell, around: Around8Cells): Cell {
+  const aliveSize = around.filter(isAliveCell).length;
+  if (isAliveCell(target)) {
+    if (aliveSize === 2 || aliveSize === 3) {
+      return "x";
+    }
+    return "-";
+  }
+  return aliveSize === 3 ? "-" : "x";
+}
+
+function createCells(
+  width: number,
+  height: number,
+  original?: Cell[][],
+): Cell[][] {
+  const newCells: Cell[][] = Array.from({ length: height }, () =>
+    Array.from({ length: width }, () => "-" as const),
   );
+  if (original === undefined) {
+    return newCells;
+  }
+
+  newCells.forEach((row, i) => {
+    row.forEach((_, j) => {
+      newCells[i][j] = original[i][j];
+    });
+  });
+  return newCells;
 }
 
 function App() {
@@ -21,14 +64,16 @@ function App() {
     const value = Number.parseInt(event.target.value);
     const width = Number.isNaN(value) ? 0 : value;
     setSize((prev) => ({ ...prev, width }));
-    setCells(createCells(width, size.height));
+  };
+
+  const cellSizeChange = () => {
+    setCells(createCells(size.width, size.height, cells));
   };
 
   const handleHeightChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = Number.parseInt(event.target.value);
     const height = Number.isNaN(value) ? 0 : value;
     setSize((prev) => ({ ...prev, height }));
-    setCells(createCells(size.width, height));
   };
 
   const [mode, setMode] = useState<Mode>("edit");
@@ -57,11 +102,19 @@ function App() {
         <div>
           <label>
             width
-            <input value={size.width} onChange={handleWidthChange} />
+            <input
+              value={size.width}
+              onChange={handleWidthChange}
+              onBlur={cellSizeChange}
+            />
           </label>
           <label>
             height
-            <input value={size.height} onChange={handleHeightChange} />
+            <input
+              value={size.height}
+              onChange={handleHeightChange}
+              onBlur={cellSizeChange}
+            />
           </label>
         </div>
         <div>
@@ -74,8 +127,8 @@ function App() {
         <div
           className={classes.cells}
           style={{
-            gridTemplateRows: `repeat(${size.height}, 1fr)`,
-            gridTemplateColumns: `repeat(${size.width}, 1fr)`,
+            gridTemplateRows: `repeat(${cells.length}, 1fr)`,
+            gridTemplateColumns: `repeat(${cells[0].length}, 1fr)`,
           }}
         >
           {cells.map((row, i) =>
@@ -85,7 +138,9 @@ function App() {
                 key={`${i}_${j}`}
                 type="button"
                 className={classes.cell}
-                style={{ backgroundColor: column === "x" ? "gray" : "white" }}
+                style={{
+                  backgroundColor: isAliveCell(column) ? "white" : "gray",
+                }}
                 onClick={() => handleCellClick(i, j)}
                 disabled={mode === "progress"}
               />
