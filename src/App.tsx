b4ea23a53864c851f8292a5893e1eed6e3fd126e
fourside <fourside@gmail.com>
2024-03-10 23:16:11 +0900

    speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Select, TextField } from "@radix-ui/themes";
+import { Button, Select, Slider, TextField } from "@radix-ui/themes";
 import { ChangeEvent, FC, useEffect, useState, useTransition } from "react";
 import classes from "./App.module.css";
 import { type Cell, createCells, evelove, isAliveCell } from "./cell";
@@ -58,6 +58,11 @@ function App() {
     setCells(createCells(size.width, size.height));
   };
 
+  const [speed, setSpeed] = useState(1);
+  const handleSpeedChange = (speed: number[]) => {
+    setSpeed(speed[0]);
+  };
+
   return (
     <main className={classes.container}>
       <div className={classes.sidemenu}>
@@ -101,6 +106,20 @@ function App() {
             </Select.Root>
           </label>
         </div>
+        <div>
+          <label>
+            speed
+            <Slider
+              defaultValue={[1]}
+              min={0.5}
+              max={3}
+              step={0.5}
+              disabled={editDisabled}
+              onValueCommit={handleSpeedChange}
+            />
+          </label>
+          x{speed}
+        </div>
         <div className={classes.buttons}>
           <Button type="button" onClick={handleModeChange}>
             {mode === "edit" ? "Start" : "Stop"}
@@ -116,7 +135,11 @@ function App() {
       </div>
       <div className={classes.board}>
         {mode === "progress" ? (
-          <EvolvingCellBoard cells={cells} onChange={handleCellChange} />
+          <EvolvingCellBoard
+            cells={cells}
+            speed={speed}
+            onChange={handleCellChange}
+          />
         ) : (
           <EditableCellBoard cells={cells} onChange={handleCellChange} />
         )}
@@ -149,23 +172,34 @@ const EditableCellBoard: FC<EditableCellBoardProps> = (props) => {
   return <CellBoard cells={props.cells} onClick={handleCellClick} />;
 };
 
+const SPEEDS_TO_MS: Record<string, number> = {
+  "0.5": 1000,
+  "1": 500,
+  "1.5": 350,
+  "2": 250,
+  "2.5": 200,
+  "3": 150,
+};
+
 type EvolvingCellBoardProps = {
   cells: Cell[][];
+  speed: number;
   onChange: (cells: Cell[][]) => void;
 };
 
 const EvolvingCellBoard: FC<EvolvingCellBoardProps> = (props) => {
   const [_, startTransition] = useTransition();
+  const ms = SPEEDS_TO_MS[props.speed.toString()];
 
   useEffect(() => {
     (async () => {
-      await sleep(500);
+      await sleep(ms);
       startTransition(() => {
         const newCells = evelove(props.cells);
         props.onChange(newCells);
       });
     })();
-  }, [props.cells, props.onChange]);
+  }, [props.cells, props.onChange, ms]);
 
   return <CellBoard cells={props.cells} />;
 };
