9a6888dfc6aff0f4581d13ed5149a9f661d9cde1
fourside <fourside@gmail.com>
2024-03-14 22:42:20 +0900

    worker build

---
+++ src/worker.ts
@@ -0,0 +1,18 @@
+import type { Fetcher, Request } from "@cloudflare/workers-types";
+
+interface Env {
+  ASSETS: Fetcher;
+}
+
+export default {
+  async fetch(request: Request, env: Env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      // TODO: Add your custom /api/* logic here.
+      return new Response("Ok");
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request);
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [react()],
+    };
+  }
+  return {
+    build: {
+      ssr: true,
+      emptyOutDir: false,
+      rollupOptions: {
+        input: "./src/worker.ts",
+        output: {
+          entryFileNames: "_worker.js",
+        },
+      },
+    },
+  };
+});
